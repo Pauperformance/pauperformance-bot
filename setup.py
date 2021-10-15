0c2e061f3b0a8bc1fec3a59257976ce27dbbfaaf
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2021-10-15 20:43:51 +0200

    Some README

--- setup.py
+++ setup.py
@@ -3,24 +3,28 @@ import os
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
+HERE = os.path.abspath(os.path.dirname(__file__))
+REQUIREMENTS_DIR = "requirements"
+README_FILE = "README.md"
+VERSION_FILE = "VERSION"
 
 
 def read_requirements(file_name):
     reqs = []
-    with open(os.path.join(here, file_name)) as in_f:
+    with open(os.path.join(HERE, file_name)) as in_f:
         for line in in_f:
             line = line.strip()
-            if not line or line.startswith("#"):
+            if not line or line.startswith("#") or line.startswith("    #") \
+                    or line.startswith("-r"):
                 continue
             reqs.append(line)
     return reqs
 
 
-with open(os.path.join(here, "README.md")) as f:
+with open(os.path.join(HERE, README_FILE)) as f:
     readme = f.read()
 
-with open(os.path.join(here, "VERSION")) as f:
+with open(os.path.join(HERE, VERSION_FILE)) as f:
     version = f.read()
 
 
@@ -36,10 +40,10 @@ setup(
     long_description_content_type="text/markdown",
     python_requires=">=3.7",
     packages=find_packages(exclude=["tests"]),
-    install_requires=read_requirements("requirements/requirements.txt"),
+    install_requires=read_requirements(f"{REQUIREMENTS_DIR}/requirements.txt"),
     extras_require={
-        "test": read_requirements("requirements/requirements-test.txt"),
-        "dev": read_requirements("requirements/requirements-dev.txt"),
+        "test": read_requirements(f"{REQUIREMENTS_DIR}/requirements-test.txt"),
+        "dev": read_requirements(f"{REQUIREMENTS_DIR}/requirements-dev.txt"),
     },
     include_package_data=True,
     package_data={
