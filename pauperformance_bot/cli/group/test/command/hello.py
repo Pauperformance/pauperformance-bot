c4594f22194634f712b97e41792e50469654a54c
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2021-10-16 12:23:10 +0200

    CLI

--- pauperformance_bot/cli/group/test/command/hello.py
+++ pauperformance_bot/cli/group/test/command/hello.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python3
-
 from pauperformance_bot.cli.builder.command import CLICommand
-from pauperformance_bot.constant.cli import (
-    ACADEMY_CLI_GROUP,
-    UPDATE_ACADEMY_CMD,
-)
+from pauperformance_bot.constant.cli import HELLO_TEST_CMD, TEST_CLI_GROUP
 from pauperformance_bot.util.log import get_application_logger
 
-logger = get_application_logger(ACADEMY_CLI_GROUP)
+logger = get_application_logger(TEST_CLI_GROUP)
 
 
-class Update(CLICommand):
+class Hello(CLICommand):
     def __init__(self):
         super().__init__(
-            UPDATE_ACADEMY_CMD,
-            "Update Pauperformance web site.",
+            HELLO_TEST_CMD,
+            "Call Myr.",
             [],
         )
 
     def dispatch_cmd(self, *args, **kwargs):
         super().dispatch_cmd(*args, **kwargs)
-        update()
+        hello()
 
 
-def update():
-    print("TODO.")
+def hello():
+    print("Myr ready to serve you, Milord!")
 
 
 if __name__ == "__main__":
-    Update().run()
+    Hello().run()
