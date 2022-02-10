db016e872e64729f0c176d740cca9520569c8c92
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2022-02-10 21:47:37 +0100

    Updated tox/doc to py38

--- pauperformance_bot/task/academy.py
+++ pauperformance_bot/task/academy.py
@@ -1,21 +1,32 @@
 from pauperformance_bot.service.academy import AcademyService
-from pauperformance_bot.service.archive.local import (
-    LocalArchiveService as LocalArchive,
+
+# from pauperformance_bot.service.archive.local import (
+#     LocalArchiveService as LocalArchive,
+# )
+from pauperformance_bot.service.archive.mtggoldfish import (
+    MTGGoldfishArchiveService,
 )
 from pauperformance_bot.service.pauperformance import PauperformanceService
-from pauperformance_bot.service.storage.local import (
-    LocalStorageService as LocalStorage,
-)
+from pauperformance_bot.service.storage.dropbox_ import DropboxService
+
+# from pauperformance_bot.service.storage.local import (
+#     LocalStorageService as LocalStorage,
+# )
 
 
 def academy_update(pauperformance):
     academy = AcademyService(pauperformance)
-    academy.update_all()
+    # academy.update_set_index()
+    # academy.update_pauper_pool()
+    # academy.update_all()
+    academy.update_archetypes()
 
 
 def main():
-    storage = LocalStorage()
-    archive = LocalArchive()
+    # storage = LocalStorage()
+    storage = DropboxService()
+    # archive = LocalArchive()
+    archive = MTGGoldfishArchiveService(storage)
     pauperformance = PauperformanceService(storage, archive)
     academy_update(pauperformance)
 
