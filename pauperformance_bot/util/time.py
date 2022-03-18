bc6ea0de70d2525003471fbd294b6ee84efa687e
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2022-03-18 16:58:27 +0100

    Testing GitHub actions

--- pauperformance_bot/util/time.py
+++ pauperformance_bot/util/time.py
@@ -11,6 +11,16 @@ def now():
     return int(round(time.time() * 1000))
 
 
+def now_utc() -> int:
+    # please note this function does not always return the same value of now(),
+    # because the local timezone (used by now()) can be different (e.g. 'CET').
+    return datetime_to_ms(datetime.utcnow())
+
+
+def datetime_to_ms(dt):
+    return int(dt.timestamp() * 1000)
+
+
 def pretty_str(now_ms: int, date_format=DEFAULT_DATE_FORMAT):
     now_dt = datetime.fromtimestamp(now_ms / 1000.0)
     return now_dt.strftime(date_format)
