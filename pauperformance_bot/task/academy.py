04a9457bd4fa848c26434c77d2695f7666390569
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2022-03-20 14:48:36 +0100

    Updates

--- pauperformance_bot/task/academy.py
+++ pauperformance_bot/task/academy.py
@@ -1,17 +1,9 @@
 from pauperformance_bot.service.academy import AcademyService
-
-# from pauperformance_bot.service.archive.local import (
-#     LocalArchiveService as LocalArchive,
-# )
 from pauperformance_bot.service.archive.mtggoldfish import (
     MTGGoldfishArchiveService,
 )
 from pauperformance_bot.service.pauperformance import PauperformanceService
 from pauperformance_bot.service.storage.dropbox_ import DropboxService
-
-# from pauperformance_bot.service.storage.local import (
-#     LocalStorageService as LocalStorage,
-# )
 
 
 def academy_update(pauperformance):
@@ -20,15 +12,13 @@ def academy_update(pauperformance):
     pauperformance.import_players_videos_from_youtube(send_notification=True)
     pauperformance.import_decks_from_deckstats(send_notification=True)
 
-    # update Academy
+    # update pages
     academy = AcademyService(pauperformance)
-    academy.update_all()
+    academy.update_all(update_dev=False)  # avoid unnecessary changes
 
 
 def main():
-    # storage = LocalStorage()
     storage = DropboxService()
-    # archive = LocalArchive()
     archive = MTGGoldfishArchiveService(storage)
     pauperformance = PauperformanceService(storage, archive)
     academy_update(pauperformance)
