aa58e18fa4e8df497fb804b4823391dd05962804
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2022-04-04 12:56:19 +0200

    More robust languages

--- pauperformance_bot/constant/flags.py
+++ pauperformance_bot/constant/flags.py
@@ -2,9 +2,10 @@ from pauperformance_bot.exceptions import UnsupportedLanguage
 
 
 def get_language_flag(language_name):
-    if language_name == "ITA" or language_name == "it":
+    language_name = language_name.lower()
+    if language_name == "ita" or language_name == "it":
         return "🇮🇹"
-    elif language_name == "ENG" or language_name == "en":
+    elif language_name == "eng" or language_name == "en":
         return "🇬🇧"
     else:
         raise UnsupportedLanguage(f"Missing flag for language {language_name}")
