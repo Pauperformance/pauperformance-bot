4863eaafca3ee8ab42731bd24931af7a60792ec6
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2022-05-21 00:24:24 +0200

    fix: investigating py39 action failure

--- pauperformance_bot/__init__.py
+++ pauperformance_bot/__init__.py
@@ -1,3 +1,6 @@
-import pkg_resources
+try:
+    import pkg_resources
+    version = pkg_resources.get_distribution(__package__).version
+except pkg_resources.DistributionNotFound:
+    version = '0.0.0'
 
-version = pkg_resources.get_distribution(__package__).version
