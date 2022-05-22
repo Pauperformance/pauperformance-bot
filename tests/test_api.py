365479baa3f2e079ca81e921af5190263a2ca707
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2022-05-22 17:41:45 +0200

    fix: dropped obsolete test

--- tests/test_api.py
+++
@@ -1,8 +0,0 @@
-from pauperformance_bot.entity.api.phd_sheet import PhDSheet
-
-
-def test_phd_sheet():
-    phd_sheet = PhDSheet("TEST")
-    print(phd_sheet)
-    print(phd_sheet.__dict__)
-    phd_sheet.dump_to_file()
