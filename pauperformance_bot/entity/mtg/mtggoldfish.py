3b9c16234a40fc96a5ef4406e4d8c4a22025f9fd
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2022-06-18 19:03:59 +0200

    feat: better deck downloader

--- pauperformance_bot/entity/mtg/mtggoldfish.py
+++ pauperformance_bot/entity/mtg/mtggoldfish.py
@@ -15,31 +15,3 @@ class MTGGoldfishTournamentSearchResult:
 
     def __hash__(self):
         return hash(self.identifier)
-
-
-@auto_repr
-@auto_str
-class MTGGoldfishTournamentDeck:
-    def __init__(
-        self,
-        *,
-        url: str,
-        archetype: str,
-        place: str,
-        pilot: str,
-        tabletop_price: int,
-        mtgo_price: int,
-    ):
-        self.url: str = url
-        self.archetype: str = archetype
-        self.place: str = place
-        self.pilot: str = pilot
-        self.tabletop_price: int = tabletop_price
-        self.mtgo_price: int = mtgo_price
-
-    @property
-    def identifier(self) -> str:
-        return self.url.rsplit("/", maxsplit=1)[-1]
-
-    def __hash__(self):
-        return hash(self.identifier)
