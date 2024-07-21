e31a301dc3c94e542d9a1a1470308f99eb467776
B++ <f.bernardi89@gmail.com>
2024-07-21 09:00:20 +0200

    wip: cards intel

--- pauperformance_bot/service/pauperformance/silver/deckstatistics.py
+++ pauperformance_bot/service/pauperformance/silver/deckstatistics.py
@@ -124,12 +124,8 @@ class DeckstatisticsFactory:
             # build a lookup dict for card stats
             for card in pd.mainboard + pd.sideboard:
                 try:
-                    sf_card = self._scryfall.get_card_named(card.card_name, fuzzy=False)
+                    self._scryfall.get_card_named(card.card_name, fuzzy=False)
                 except CardNotFoundException:
-                    logger.error(f"Card not found in scryfall {card.card_name}")
-                    continue
-
-                if not sf_card:
                     logger.error(f"Card not found in scryfall {card.card_name}")
                     continue
 
