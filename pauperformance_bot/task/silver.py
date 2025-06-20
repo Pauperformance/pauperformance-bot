123ec355579f14e56bf18a8926798754a52134ec
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2025-06-20 10:54:40 +0200

    Gunicorn server

--- pauperformance_bot/task/silver.py
+++ pauperformance_bot/task/silver.py
@@ -35,6 +35,18 @@ def main(input_file, output_file):
     generate_dpl_meta(input_file, output_file)
 
 
+def dpl_classifier(environ, start_response):
+    """Simplest possible application object"""
+    data = b'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
+
+
 if __name__ == "__main__":
     main(
         posix_path(
