b3cfa2923169ea446c57b1645f1be5cd71d37c38
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2025-12-22 13:15:42 +0100

    Testing skip-secrets on PRs

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,27 +2,17 @@ import os
 
 import pytest
 
-REQUIRED_SECRETS = [
-    "DISCORD_BOT_TOKEN",
-    "DROPBOX_APP_KEY",
-    "DROPBOX_APP_SECRET",
-    "DROPBOX_REFRESH_TOKEN",
-    "MTGGOLDFISH_PAUPERFORMANCE_PASSWORD",
-    "MTGGOLDFISH_PAUPERFORMANCE_USERNAME",
-    "TWITCH_APP_CLIENT_ID",
-    "TWITCH_APP_CLIENT_SECRET",
-    "YOUTUBE_API_KEY",
-]
+REQUIRED_SECRETS = "RUN_TESTS_WITH_SECRETS"
 
 
 def pytest_collection_modifyitems(config, items):
-    missing = [s for s in REQUIRED_SECRETS if not os.getenv(s)]
-
-    if not missing:
+    if os.getenv(REQUIRED_SECRETS):
         return
 
-    skip_marker = pytest.mark.skip(reason=f"Missing CI secrets: {', '.join(missing)}")
+    skip_marker = pytest.mark.skip(
+        reason=f"Excluding tests with secrets (no : {REQUIRED_SECRETS} found)"
+    )
 
     for item in items:
-        if "integration" in item.keywords:
+        if "secrets" in item.keywords:
             item.add_marker(skip_marker)
--- tests/test_dropbox_.py
+++ tests/test_dropbox_.py
@@ -3,14 +3,14 @@ import pytest
 from pauperformance_bot.service.pauperformance.storage.dropbox_ import DropboxService
 
 
-@pytest.mark.integration
+@pytest.mark.secrets
 def test_list_imported_youtube_videos():
     assert any(
         "Pauperformance" in v for v in DropboxService().list_imported_youtube_videos()
     )
 
 
-@pytest.mark.integration
+@pytest.mark.secrets
 def test_list_imported_mtggoldfish_deck_names():
     assert any(
         "Jund" in d for d in DropboxService().list_imported_mtggoldfish_deck_names()
